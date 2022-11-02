8dda218522f4565998c9579d422cd6e13dfa9aa1
InJun2 <os2864@naver.com>
2022-11-02 17:06:22 +0900

    - ItemState에 따른 경매 진행 수정 - 처음 경매 진행 시 ItemState 경매 진행 상태 Progress로 변경

--- src/main/java/com/selab/auction/error/dto/ErrorMessage.java
+++ src/main/java/com/selab/auction/error/dto/ErrorMessage.java
@@ -10,7 +10,12 @@ public enum ErrorMessage {
     NOT_EXIST_ITEM(HttpStatus.NOT_FOUND, "존재하지 않는 상품입니다"),
     NOT_EXITS_CATEGORY(HttpStatus.NOT_FOUND, "존재하지 않는 카테고리입니다"),
     DELETE_ITEM(HttpStatus.NOT_FOUND, "삭제된 상품입니다"),
+    WRONG_ITEM_STATE(HttpStatus.BAD_REQUEST, "잘못된 아이템 정보입니다"),
     WRONG_REQUEST_PRICE(HttpStatus.BAD_REQUEST, "잘못된 경매 요청가격입니다"),
+    COMPLETED_AUCTION(HttpStatus.BAD_REQUEST, "이미 경매가 끝난 상품입니다"),
+
+
+
     ;
 
     private final HttpStatus status;
