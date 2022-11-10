ee98d0eaf2fb2e3794234c9e68e6ebed50de424b
sonrose <kmslove29@gmail.com>
2022-11-10 19:03:53 +0900

    ✨ [feature/commonmodule-18] Jacoco 도입, Error 발생 시 Slack 알림 기능 구현

--- src/main/java/com/selab/auction/member/service/MemberSignUpService.java
+++ src/main/java/com/selab/auction/member/service/MemberSignUpService.java
@@ -8,6 +8,7 @@ import com.selab.auction.member.model.dto.MemberSignUpResponseDto;
 import com.selab.auction.member.model.entity.Member;
 import com.selab.auction.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
