1d01723788f2f09ca6f467ae4b40f01219312a10
sonrose <kmslove29@gmail.com>
2022-11-10 23:28:23 +0900

    ✨ [feature/cors-36] CORS 설정

--- src/main/java/com/selab/auction/item/controller/ItemController.java
+++ src/main/java/com/selab/auction/item/controller/ItemController.java
@@ -10,16 +10,9 @@ import com.selab.auction.item.service.ItemService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/items")
