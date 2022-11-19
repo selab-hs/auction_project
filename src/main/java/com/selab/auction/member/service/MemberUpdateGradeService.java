37fb76908088dbf808c60d31d31a0f654bca9333
sonrose <kmslove29@gmail.com>
2022-11-19 18:24:20 +0900

    🚚 [move/updateMemberGrade-48] AuctionCommentService 클래스의 updateMemberGrade Method를 member service package로 옮긴다.

---
+++ src/main/java/com/selab/auction/member/service/MemberUpdateGradeService.java
@@ -0,0 +1,20 @@
+package com.selab.auction.member.service;
+
+import com.selab.auction.error.exception.member.find.NotExistMemberException;
+import com.selab.auction.member.model.entity.Member;
+import com.selab.auction.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberUpdateGradeService {
+    private final MemberRepository memberRepository;
+
+    public void updateMemberGrade(long memberId, double grade){
+        Member member = memberRepository.findById(memberId).orElseThrow(NotExistMemberException::new);
+        member.updateMemberGrade(grade);
+    }
+}
--- src/main/java/com/selab/auction/participation/service/AuctionCommentService.java
+++ src/main/java/com/selab/auction/participation/service/AuctionCommentService.java
@@ -1,9 +1,7 @@
 package com.selab.auction.participation.service;
 
-import com.selab.auction.error.exception.member.find.NotExistMemberException;
 import com.selab.auction.item.service.ItemService;
-import com.selab.auction.member.model.entity.Member;
-import com.selab.auction.member.repository.MemberRepository;
+import com.selab.auction.member.service.MemberUpdateGradeService;
 import com.selab.auction.participation.model.dto.AuctionBuyCommentResponseDto;
 import com.selab.auction.participation.model.dto.AuctionSaleCommentResponseDto;
 import com.selab.auction.participation.model.dto.CreateBuyCommentDto;
@@ -24,16 +22,14 @@ public class AuctionCommentService{
     private final AuctionBuyCommentRepository buyRepository;
     private final AuctionSaleCommentRepository saleRepository;
     private final ItemService itemService;
-
-    private final MemberRepository memberRepository;
-
+    private final MemberUpdateGradeService memberUpdateGradeService;
 
     @Transactional
     public AuctionBuyCommentResponseDto registerBuyComment(CreateBuyCommentDto commentDto) {
         long memberId = itemService.getItemEntityById(commentDto.getItemId()).getMemberId();
         AuctionBuyComment auctionBuyComment = buyRepository.saveAndFlush(commentDto.toEntity(memberId));
 
-        updateMemberGrade(memberId, calculationMemberGrade(memberId));
+        memberUpdateGradeService.updateMemberGrade(memberId, calculationMemberGrade(memberId));
 
         return auctionBuyComment.toResponseDto();
     }
@@ -42,15 +38,9 @@ public class AuctionCommentService{
     public AuctionSaleCommentResponseDto registerSaleComment(CreateSaleCommentDto commentDto) {
         AuctionSaleComment auctionSaleComment = saleRepository.saveAndFlush(commentDto.toEntity());
 
-        updateMemberGrade(auctionSaleComment.getSaleMemberId(), calculationMemberGrade(auctionSaleComment.getSaleMemberId()));
+        memberUpdateGradeService.updateMemberGrade(auctionSaleComment.getSaleMemberId(), calculationMemberGrade(auctionSaleComment.getSaleMemberId()));
 
         return auctionSaleComment.toResponseDto();
-    }
-
-    // 해당 메소드는 MemberService로 이동해놓을 것
-    public void updateMemberGrade(long memberId, double grade){
-        Member member = memberRepository.findById(memberId).orElseThrow(NotExistMemberException::new);
-        member.updateMemberGrade(grade);
     }
 
     @Transactional(readOnly = true)
