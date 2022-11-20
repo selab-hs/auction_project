9bbb684219afadce4ecf4bc3659021ffb95f32f8
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-11-20 22:40:48 +0900

    fix: item 도메인 수정

--- src/main/java/com/selab/auction/item/service/ItemService.java
+++ src/main/java/com/selab/auction/item/service/ItemService.java
@@ -7,15 +7,13 @@ import com.selab.auction.item.model.dto.ItemResponse;
 import com.selab.auction.item.model.dto.ItemUpdateRequest;
 import com.selab.auction.item.model.dto.ItemsResponse;
 import com.selab.auction.item.model.entity.Item;
-import com.selab.auction.item.model.vo.Category;
 import com.selab.auction.item.model.vo.ItemState;
 import com.selab.auction.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,16 +22,16 @@ public class ItemService {
     private final ItemRepository itemRepository;
 
     @Transactional(readOnly = true)
-    public ItemsResponse getAllItems() {
-        List<Item> items = itemRepository.findAll();
-        List<ItemResponse> responses = items.stream().filter(item -> !item.getState().equals(ItemState.INACTIVE)).map(ItemResponse::of).collect(Collectors.toList());
-        return new ItemsResponse(responses);
+    public Page<ItemResponse> getAllItems(Pageable pageable) {
+        return itemRepository.findAllByStateNot(pageable, ItemState.INACTIVE)
+                .map(ItemResponse::of);
     }
 
+    // TODO : JPA에서 조회할때부터 상태값 조회하기
     @Transactional(readOnly = true)
     public ItemResponse getItemById(Long id) {
         Item item = itemRepository.findById(id).orElseThrow(NotExistItemException::new);
-        if(item.getState().equals(ItemState.INACTIVE)) {
+        if (item.getState().equals(ItemState.INACTIVE)) {
             throw new DeleteItemException();
         }
         return ItemResponse.of(item);
