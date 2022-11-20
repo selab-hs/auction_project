1b3d19f94dc0e199dede9dd6ae1c8a8a35c08f3b
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-11-20 22:47:47 +0900

    fix: auction 도메인 수정

--- src/main/java/com/selab/auction/participation/service/AuctionServiceImpl.java
+++ src/main/java/com/selab/auction/participation/service/AuctionServiceImpl.java
@@ -9,10 +9,8 @@ import com.selab.auction.item.model.entity.Item;
 import com.selab.auction.item.model.vo.ItemState;
 import com.selab.auction.item.service.ItemService;
 import com.selab.auction.member.service.MemberFindService;
-import com.selab.auction.participation.model.dto.AuctionBuyCommentResponseDto;
-import com.selab.auction.participation.model.dto.CreateAuctionDto;
 import com.selab.auction.participation.model.dto.AuctionResponseDto;
-import com.selab.auction.participation.model.dto.CreateBuyCommentDto;
+import com.selab.auction.participation.model.dto.CreateAuctionDto;
 import com.selab.auction.participation.repository.AuctionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,9 +18,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 
+// TODO : 코드 리펙토링 필요
 @Service
 @RequiredArgsConstructor
-public class AuctionServiceImpl implements AuctionService{
+public class AuctionServiceImpl implements AuctionService {
     private final AuctionRepository auctionRepository;
     private final MemberFindService memberFindService;
     private final ItemService itemService;
@@ -33,19 +32,12 @@ public class AuctionServiceImpl implements AuctionService{
         memberFindService.findById(createAuctionDto.getMemberId());
         Item item = itemService.getItemEntityById(createAuctionDto.getItemId());
 
-        if(item.getState().equals(ItemState.ACTIVE)){
-            return validateFirstItemPrice(createAuctionDto, item);
-        }
-
-        if(item.getState().equals(ItemState.PROGRESS)) {
-            return validateItemPrice(createAuctionDto, item);
-        }
-
-        if(item.getState().equals(ItemState.COMPLETE)) {
-            throw new CompletedAuctionException();
-        }
-
-        throw new WrongItemStateException();
+        return switch (item.getState()) {
+            case ACTIVE -> validateFirstItemPrice(createAuctionDto, item);
+            case PROGRESS -> validateItemPrice(createAuctionDto, item);
+            case COMPLETE -> throw new CompletedAuctionException();
+            default -> throw new WrongItemStateException();
+        };
     }
 
     @Override
@@ -53,10 +45,10 @@ public class AuctionServiceImpl implements AuctionService{
         return auctionRepository.findById(id).orElseThrow(WrongAuctionIdException::new).toResponseDto();
     }
 
-    private AuctionResponseDto validateItemPrice(CreateAuctionDto createAuctionDto, Item item){
+    private AuctionResponseDto validateItemPrice(CreateAuctionDto createAuctionDto, Item item) {
         Auction response = auctionRepository.findByItemId(item.getId()).stream()
                 .max(Comparator.comparing(Auction::getAuctionPrice))
-                .filter((auction)->createAuctionDto.getRequestPrice() > (auction.getAuctionPrice() + item.getPrice() * 0.01))
+                .filter((auction) -> createAuctionDto.getRequestPrice() > (auction.getAuctionPrice() + item.getPrice() * 0.01))
                 .orElseThrow(WrongRequestPriceException::new);
 
         auctionRepository.save(response);
@@ -64,8 +56,8 @@ public class AuctionServiceImpl implements AuctionService{
         return response.toResponseDto();
     }
 
-    private AuctionResponseDto validateFirstItemPrice(CreateAuctionDto createAuctionDto, Item item){
-        if(createAuctionDto.getRequestPrice() >= item.getPrice()){
+    private AuctionResponseDto validateFirstItemPrice(CreateAuctionDto createAuctionDto, Item item) {
+        if (createAuctionDto.getRequestPrice() >= item.getPrice()) {
             Auction response = auctionRepository.save(createAuctionDto.toEntity());
             item.updateState(ItemState.PROGRESS);
 
