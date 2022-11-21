0752cc442bc7acd3d9f7d8ce477f071e1fc53c34
sonrose <kmslove29@gmail.com>
2022-11-21 15:52:37 +0900

    ✨ [feature/login-41] 로그인 기능 구현 (오류 수정)

--- src/main/java/com/selab/auction/member/controller/MemberController.java
+++ src/main/java/com/selab/auction/member/controller/MemberController.java
@@ -8,7 +8,6 @@ import com.selab.auction.member.signin.service.MemberSignInService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
