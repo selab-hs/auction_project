c42caa8ebb58f4fb4d7fde3605a958b79653eaee
InJun2 <os2864@naver.com>
2022-11-26 22:58:50 +0900

    - ItemService 경매 상태 완료 변경, 경매 진행 중 조회 메소드 추가 - schedulerService 추가. 경매 기간 만료시 스케줄링 처리로 만료. 10분마다 실행

---
+++ src/main/java/com/selab/auction/scheduler/service/SchedulerService.java
@@ -0,0 +1,36 @@
+package com.selab.auction.scheduler.service;
+
+import com.selab.auction.item.model.entity.Item;
+import com.selab.auction.item.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SchedulerService {
+
+    private final ItemService itemService;
+
+    @Scheduled(cron = "0 0/10 * * * *")
+    public void runAfterTenMinuteRepeatItemPeriodValidation() {
+        log.info("10분 주기 아이템 경매 기간 점검 실행 시간  => time : " + LocalTime.now());
+
+        List<Item> items = itemService.getItemsEntityByItemStateProgress();
+
+        items.stream()
+                .filter((item) -> LocalDateTime.now().isAfter(
+                        item.getCreatedAt().plusDays(item.getAuctionPeriod())
+                ))
+                .peek((item) -> log.info(item.getId() + "번 아이템 경매 기간 만료"))
+                .forEach(itemService::updateItemStateToCompleted);
+
+        // 이후 경매 기간이 지난 아이템의 경우 update진행
+    }
+}
